1c2037870ba3545de2bb1260b26189e4bf15b339
Leanne <leannestranks86@hotmail.com>
2021-01-26 18:26:19 +1000

    Added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get '/services', to: 'services#index'
+    post '/services', to: 'services#create'
+    get '/services/:id', to: "services#show"
+    put '/services/:id', to: 'services#update'
+    delete '/services/:id', to: 'services#destroy'
+
+    get '/bookings', to: 'bookings#index'
+    post '/bookings', to: 'booking#create'
+    get '/bookings/:id', to: "bookings#show"
+    put '/bookings/:id', to: 'bookings#update'
+    delete '/bookings/:id', to: 'bookings#destroy'
 end
