649679aa46be9b098d0873ff5eb4edc17c96e4f0
Leanne <leannestranks86@hotmail.com>
2021-01-26 19:47:14 +1000

    Updated

--- db/migrate/20210126093554_create_services.rb
+++ db/migrate/20210126093554_create_services.rb
@@ -1,8 +1,8 @@
 class CreateServices < ActiveRecord::Migration[6.0]
   def change
     create_table :services do |t|
-      t.string :service
-      t.string :price
+      t.string :service_name
+      t.string :service_price
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
