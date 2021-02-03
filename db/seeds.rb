27964cbf54624e877f57758cf016f4369082276c
Leanne <leannestranks86@hotmail.com>
2021-02-04 06:45:37 +1000

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Suburb.destroy_all()
 Postcode.destroy_all()
+Suburb.destroy_all()
 Service.destroy_all()
 Category.destroy_all()
 Booking.destroy_all()
